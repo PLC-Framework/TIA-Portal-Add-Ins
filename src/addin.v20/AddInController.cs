41c83a52573d3d40e81fbb47b032a55dcebaac44
cyanezf <cyanezf@plcframework.com>
2026-08-23 17:45:40 +0200

    Feat: add controller, provider, edit addin.v20.csproj

---
+++ src/addin.v20/AddInController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+using Siemens.Engineering;
+using Siemens.Engineering.AddIn.Menu;
+
+namespace addin
+{
+    public class AddInController : ContextMenuAddIn
+    {
+        private const string Title = "PLC-Framework";
+        private readonly TiaPortal _tiaPortal;
+
+        public AddInController(TiaPortal tiaPortal) : base(Title)
+        {
+            _tiaPortal = tiaPortal;
+        }
+
+        protected override void BuildContextMenuItems(ContextMenuAddInRoot addInRootSubmenu)
+        {
+            addInRootSubmenu.Items.AddActionItem<Project> (
+                "Hello world!",
+                (MenuSelectionProvider<Project> menuSelectionProvider) =>
+                {
+                    Project project = menuSelectionProvider?.GetSelection<Project>().FirstOrDefault();
+                    string projectName = project?.Name ?? "No project";
+
+                    _tiaPortal.GetMessageBox().ShowNotification(NotificationIcon.Information, Title, $"Hello world! {projectName}");
+                });
+        }
+    }
+}
