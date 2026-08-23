11ddb60b050af7d740912c623b39c8d71fb7d396
cyanezf <cyanezf@plcframework.com>
2026-08-23 19:02:29 +0200

    feat: create addin.v21

---
+++ src/addin.v21/AddInProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using Siemens.Engineering;
+using Siemens.Engineering.AddIn;
+using Siemens.Engineering.AddIn.Menu;
+
+namespace addin
+{
+    public class AddInProvider : ProjectTreeAddInProvider
+    {
+        private readonly TiaPortal _tiaPortal;
+
+        public AddInProvider(TiaPortal tiaPortal)
+        {
+            _tiaPortal = tiaPortal;
+        }
+
+        protected override IEnumerable<ContextMenuAddIn> GetContextMenuAddIns()
+        {
+            yield return new AddInController(_tiaPortal);
+        }
+    }
+}
