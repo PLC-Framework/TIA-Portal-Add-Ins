ae534a15124f9a3772a0234e6ba10accbda383f1
cyanezf <cyanezf@plcframework.com>
2026-09-04 19:52:25 +0200

    feat: replace PlcWebApi with S7PlcWebserverApi and add Satellite.DataBlockSnapshot project files

---
+++ src/Satellite.DataBlockSnapshot/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UI.Shared;
+
+namespace Satellite.DataBlockSnapshot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (!SingleInstance.Claim("Satellite.DataBlockSnapshot"))
+            {
+                Shutdown();
+                return;
+            }
+
+            new MainWindow().Show();
+        }
+    }
+}
