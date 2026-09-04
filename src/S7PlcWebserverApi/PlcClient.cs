5074abc8c6c72ecb488b3b642e67ee0f6a2989ed
cyanezf <cyanezf@plcframework.com>
2026-09-04 20:02:58 +0200

    feat: add PlcClient

---
+++ src/S7PlcWebserverApi/PlcClient.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace S7PlcWebserverApi
+{
+    public sealed class PlcClient : IDisposable
+    {
+        public PlcClient(string ip, string user, string password);
+
+        public string Ip { get; }
+        public bool IsLoggedIn { get; }
+        public void Login();
+        public void Logout();
+
+        internal JToken Rpc(string method, JObject parameters = null);
+    }
+}
